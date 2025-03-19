1f30b0dface0e5a3ef69edc8bb0be30cb37fdb71
Leonhard Tilly <leonhard.tilly@eq-3.com>
2025-03-19 15:42:37 +0100

    Dismissible 6.3 start

--- lib/screens/birthday_screen/birthday.screen.dart
+++ lib/screens/birthday_screen/birthday.screen.dart
@@ -39,124 +39,130 @@ class BirthdayScreenState extends State<BirthdayScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 250, 250),
-      body: ListView.builder(
-        itemCount: birthdays.length,
-        itemBuilder: (context, index) {
-          final birthday = birthdays[index];
-          return ListTile(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                BirthdayDetailScreen.routeName,
-                arguments: birthday,
-              ).then((_) {
-                loadBirthdays(); //Aktualisiert die Liste nach Rückkehr
-              });
-            },
-            title: Container(
-              width: 100,
-              height: 60,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 73, 219, 224), // Sonst Orange
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(78, 0, 0, 0),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(2, 2),
+      body: Dismissible(
+        key: UniqueKey(),
+        direction: DismissDirection.endToStart,
+        child: ListView.builder(
+          itemCount: birthdays.length,
+          itemBuilder: (context, index) {
+            final birthday = birthdays[index];
+            return ListTile(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  BirthdayDetailScreen.routeName,
+                  arguments: birthday,
+                ).then((_) {
+                  loadBirthdays(); //Aktualisiert die Liste nach Rückkehr
+                });
+              },
+              title: Container(
+                width: 100,
+                height: 60,
+                decoration: BoxDecoration(
+                  color:
+                      const Color.fromARGB(255, 73, 219, 224), // Sonst Orange
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(78, 0, 0, 0),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
                   ),
-                ],
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(0, 0, 0, 0),
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          width: 1,
+                        ),
+                      ),
+                      child: (birthday.profileImage == null)
+                          ? AvatarPlus(
+                              birthday.id,
+                              height: MediaQuery.of(context).size.width - 350,
+                              width: MediaQuery.of(context).size.width - 350,
+                            )
+                          : Image.network(
+                              birthday.profileImage!,
+                              height: MediaQuery.of(context).size.width - 370,
+                              width: MediaQuery.of(context).size.width - 370,
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                    const SizedBox(width: 15),
+                    IntrinsicWidth(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              birthday.name,
+                              style: Theme.of(context).textTheme.titleLarge,
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              "wird ${DateFormat('EE, d. MMMM', 'de_DE').format(birthday.nextBirthdayDate)} ${birthday.age + 1} Jahre alt",
+                              style: Theme.of(context).textTheme.bodySmall,
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    const Spacer(),
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: (birthday.nextBirthday ?? 999) < 5
+                              ? const Color.fromARGB(
+                                  255, 255, 165, 0) // Weniger als 10 Tage
+                              : const Color.fromARGB(
+                                  255, 129, 152, 221), // Sonst Orange
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 5, right: 5, top: 3, bottom: 4),
+                          child: Text(
+                            "${birthday.nextBirthday.toString()} \n Tage",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(
+                                    height: 1), // Reduziert Zeilenabstand
+                            textAlign: TextAlign.center,
+                            //extAlign: TextAlign.right
+                          ),
+                        )),
+                    IconButton(
+                      onPressed: () {
+                        shareBirthdayAsCalendarEvent(birthday);
+                      },
+                      icon: Icon(Icons.adaptive.share),
+                    ),
+                  ],
                 ),
               ),
-              child: Row(
-                children: [
-                  const SizedBox(width: 8),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      borderRadius: BorderRadius.circular(25),
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        width: 1,
-                      ),
-                    ),
-                    child: (birthday.profileImage == null)
-                        ? AvatarPlus(
-                            birthday.id,
-                            height: MediaQuery.of(context).size.width - 350,
-                            width: MediaQuery.of(context).size.width - 350,
-                          )
-                        : Image.network(
-                            birthday.profileImage!,
-                            height: MediaQuery.of(context).size.width - 370,
-                            width: MediaQuery.of(context).size.width - 370,
-                            fit: BoxFit.cover,
-                          ),
-                  ),
-                  const SizedBox(width: 15),
-                  IntrinsicWidth(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            birthday.name,
-                            style: Theme.of(context).textTheme.titleLarge,
-                            textAlign: TextAlign.left,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            "wird ${DateFormat('EE, d. MMMM', 'de_DE').format(birthday.nextBirthdayDate)} ${birthday.age + 1} Jahre alt",
-                            style: Theme.of(context).textTheme.bodySmall,
-                            textAlign: TextAlign.left,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  const Spacer(),
-                  Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: (birthday.nextBirthday ?? 999) < 5
-                            ? const Color.fromARGB(
-                                255, 255, 165, 0) // Weniger als 10 Tage
-                            : const Color.fromARGB(
-                                255, 129, 152, 221), // Sonst Orange
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 5, right: 5, top: 3, bottom: 4),
-                        child: Text(
-                          "${birthday.nextBirthday.toString()} \n Tage",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium
-                              ?.copyWith(height: 1), // Reduziert Zeilenabstand
-                          textAlign: TextAlign.center,
-                          //extAlign: TextAlign.right
-                        ),
-                      )),
-                  IconButton(
-                    onPressed: () {
-                      shareBirthdayAsCalendarEvent(birthday);
-                    },
-                    icon: Icon(Icons.adaptive.share),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
